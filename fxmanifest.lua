4feadfa3ded3e751f9873e0e19be52ebf2372589
Juanjo <74838696+string-null@users.noreply.github.com>
2021-10-12 21:42:19 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+game 'gta5'
+
+fx_version 'adamant'
+
+version '1.0.0' -- DON'T TOUCH THIS
+
+client_script 'main.lua'
+
+server_script 'version_check.lua'
+
+ui_page 'ui.html'
+
+files {
+   'ui.html'
+}
+
+description 'NPCarhud, Frontend By Zesk#1801 And Back-end By Juanjo#7253 | https://discord.gg/48vurnuCmm'
+
+author 'Zesk#1801 & Juanjo#7253'
