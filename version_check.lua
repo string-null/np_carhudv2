082b39e2278a4e216ca2e91ada5cf573d78883c4
Juanjo <74838696+string-null@users.noreply.github.com>
2021-10-12 21:28:22 +0200

    Update version_check.lua

--- version_check.lua
+++ version_check.lua
@@ -4,7 +4,7 @@ Citizen.CreateThread(function()
     local current_version = GetResourceMetadata(resource_name, 'version', 0)
     PerformHttpRequest('https://raw.githubusercontent.com/string-null/Nonpayer_Versions/master/'..resource_name..'.txt',function(error, result, headers)
         local new_version = result:sub(1, -2)
-        SetConvarServerInfo("np_carhud2 [by Juanjo#7253]", current_version)
+        SetConvarServerInfo("np_carhudv2 [by Juanjo#7253]", current_version)
         if new_version ~= current_version then
             print('^2['..resource_name..'] - New Update Available.^0\nCurrent Version: ^5'..current_version..'^0\nNew Version: ^5'..new_version..'^0')
         end
