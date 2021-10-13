f96e3fd3c8b78d4d23d047753b6808e20f288e7e
visibait <33099726+visibait@users.noreply.github.com>
2021-10-13 21:50:51 +0200

    refactor(client/main): Removed ESX and code summarize.

--- main.lua
+++ main.lua
@@ -1,62 +1,37 @@
-ESX = nil
-
 local cruiser
-
-Citizen.CreateThread(function() 
-    while ESX == nil do 
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end) 
-        Citizen.Wait(350) 
-    end
-end)
 
 Citizen.CreateThread(function()
 	while true do
-		local ped = GetPlayerPed(-1)
-		local veh = GetVehiclePedIsIn(ped, false)
+        Citizen.Wait(0)
+		local ped = PlayerPedId()
 		local sleep = true
-		Citizen.Wait(100)
-		if veh then
-			sleep = false
-			local speed = GetEntitySpeed(veh)*3.6 -- Value in KM/H
-			local gear = GetVehicleCurrentGear(veh)
-			SendNUIMessage({
-				isInCoche = veh;
+        local vehicle = GetVehiclePedIsIn(ped, false)
+		if IsPedInAnyVehicle(ped, false) then
+			local speed = GetEntitySpeed(vehicle) * 3.6 -- Value in KM/H
+            local health = GetVehicleEngineHealth(vehicle)
+            local fuel = GetVehicleFuelLevel(vehicle)
+            if GetIsVehicleEngineRunning(vehicle) and IsControlJustPressed(1, 243) and GetPedInVehicleSeat(vehicle, player) then
+                if cruiser == 'on' then
+                    cruiser = 'off'
+                    SetEntityMaxSpeed(vehicle, GetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel"))
+                else
+                    cruiser = 'on'
+                    SetEntityMaxSpeed(vehicle, speed)
+                end
+            end
+            -- Send all the data to the NUI browser.
+            SendNUIMessage({
+				isInCoche = vehicle;
 				speed = speed;
-				vidatexto = ESX.Math.Round(GetVehicleEngineHealth(veh) / 10),
-				fueltexto = ESX.Math.Round(GetVehicleFuelLevel(veh)),
+                vidatexto = math.floor(health + 0.5)
+				fueltexto = math.floor(fuel + 0.5),
 			})
+            sleep = false
 		else
 			SendNUIMessage({
-				isInCoche = veh;
+				isInCoche = vehicle;
 			})
 		end
 		if sleep then Citizen.Wait(1200) end
-	end
-end)
-
-Citizen.CreateThread(function()
-
-	while true do
-		Citizen.Wait(450)
-
-		local player = GetPlayerPed(-1)
-		local vehicle = GetVehiclePedIsIn(player, false)
-		local vehicleClass = GetVehicleClass(vehicle)
-
-		-- Vehicle Cruiser
-		if IsPedInAnyVehicle(player, false) and GetIsVehicleEngineRunning(vehicle) and IsControlJustPressed(1, 243) and GetPedInVehicleSeat(vehicle, player) then
-			
-			local vehicleSpeedSource = GetEntitySpeed(vehicle)
-
-			if vehicleCruiser == 'on' then
-				vehicleCruiser = 'off'
-				SetEntityMaxSpeed(vehicle, GetVehicleHandlingFloat(vehicle,"CHandlingData","fInitialDriveMaxFlatVel"))
-				
-			else
-				vehicleCruiser = 'on'
-				SetEntityMaxSpeed(vehicle, vehicleSpeedSource)
-			end
-        end
-
 	end
 end)
